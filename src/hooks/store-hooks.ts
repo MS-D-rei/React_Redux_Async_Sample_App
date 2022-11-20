dffc13ff637df283b20daeea1687e1bb763b8cb9
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-20 18:12:09 +0900

    add store-hooks

---
+++ src/hooks/store-hooks.ts
@@ -0,0 +1,8 @@
+// https://redux.js.org/tutorials/typescript-quick-start
+
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '@/store/store';
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
