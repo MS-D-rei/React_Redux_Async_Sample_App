f98dbeda6a2b746d75c0b55bf0ce7babf9a53e2b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-20 18:32:38 +0900

    add isCartVisible condition rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import Layout from "@/components/Layout/Layout";
-import Cart from "@/components/Cart/Cart";
-import Products from "@/components/Shop/Products";
+import Layout from '@/components/Layout/Layout';
+import Cart from '@/components/Cart/Cart';
+import Products from '@/components/Shop/Products';
+import { useAppSelector } from './hooks/store-hooks';
 
 function App() {
+  const isCartVisible = useAppSelector((state) => state.ui.isCartVisible);
   return (
     <Layout>
-      <Cart />
+      {isCartVisible && <Cart />}
       <Products />
     </Layout>
-  )
+  );
 }
 
 export default App;
--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,8 +1,17 @@
 import { CartButtonBadgeSpan, CartButtonButton } from "@/components/Cart/CartButtonStyle";
+import { useAppDispatch, useAppSelector } from "@/hooks/store-hooks";
+import { toggle } from "@/store/uiSlice";
 
 function CartButton() {
+  const dispatch = useAppDispatch();
+  const isCartVisible = useAppSelector((state) => state.ui.isCartVisible)
+
+  const toggleCartHandler = () => {
+    dispatch(toggle());
+  }
+
   return (
-    <CartButtonButton>
+    <CartButtonButton onClick={toggleCartHandler}>
       <span>My Cart</span>
       <CartButtonBadgeSpan>1</CartButtonBadgeSpan>
     </CartButtonButton>
