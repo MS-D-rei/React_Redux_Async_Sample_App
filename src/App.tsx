67a444a8016410a2558da9f5a32b842c1c9fa224
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-11-22 18:54:24 +0900

    add firebase PUT request with useEffect  in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import Layout from '@/components/Layout/Layout';
 import Cart from '@/components/Cart/Cart';
 import Products from '@/components/Shop/Products';
 import { useAppSelector } from './hooks/store-hooks';
+import { useEffect } from 'react';
+
+const firebaseURL =
+  'https://react-httprequest-sample-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json';
 
 function App() {
   const isCartVisible = useAppSelector((state) => state.ui.isCartVisible);
+  const cart = useAppSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(firebaseURL, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {isCartVisible && <Cart />}
